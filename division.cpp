1e7797d97d8723c153b291e5dbf8f689d9db13a0
JostinCevallos <jcevallos@alumno.montepiedra.edu.ec>
2025-10-06 08:53:38 -0500

    Add files via upload

---
+++ division.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//división entre dos números enteros
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"tienes que introducir dos números enteros"<<endl;
+    int a;
+    int b;
+   cout<<"el programa hará la división de estos dos números" <<endl;
+   cin>>a;
+   cin>>b;
+   int div=(a)/(b);
+   cout<<div;
+   return 0;
+   
+}
