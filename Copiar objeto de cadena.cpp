3d238c49d73b67ae68f539837865ecb945184058
JostinCevallos <jcevallos@alumno.montepiedra.edu.ec>
2025-10-13 09:09:56 -0500

    Add files via upload

---
+++ Copiar objeto de cadena.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s1, s2;
+
+    cout << "Enter string s1: ";
+    getline (cin, s1);
+
+    s2 = s1;
+
+    cout << "s1 = "<< s1 << endl;
+    cout << "s2 = "<< s2;
+
+    return 0;
+}
