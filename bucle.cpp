254b1ac28fd48e48870a27c8bd99ed53c066a22f
JostinCevallos <jcevallos@alumno.montepiedra.edu.ec>
2025-10-18 16:24:34 -0500

    Add files via upload

---
+++ bucle.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
